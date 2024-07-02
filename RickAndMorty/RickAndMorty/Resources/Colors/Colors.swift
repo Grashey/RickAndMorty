8204f4e84d21c548d27f3461d68f801d6b0087fa
Grashey <grashey@mail.ru>
2024-07-02 13:13:08 +0300

    Добавил палитру

---
+++ RickAndMorty/RickAndMorty/Resources/Colors/Colors.swift
@@ -0,0 +1,40 @@
+//
+//  Colors.swift
+//  RickAndMorty
+//
+//  Created by Aleksandr Fetisov on 02.07.2024.
+//
+
+import UIKit
+
+@objc
+extension UIColor {
+    
+    @objc public class var rm_background: UIColor {
+        return UIColor(named: #function, in: .main, compatibleWith: nil)!
+    }
+    
+    @objc public class var rm_black: UIColor {
+        return UIColor(named: #function, in: .main, compatibleWith: nil)!
+    }
+    
+    @objc public class var rm_dropdownArrow: UIColor {
+        return UIColor(named: #function, in: .main, compatibleWith: nil)!
+    }
+    
+    @objc public class var rm_gray: UIColor {
+        return UIColor(named: #function, in: .main, compatibleWith: nil)!
+    }
+    
+    @objc public class var rm_green: UIColor {
+        return UIColor(named: #function, in: .main, compatibleWith: nil)!
+    }
+    
+    @objc public class var rm_red: UIColor {
+        return UIColor(named: #function, in: .main, compatibleWith: nil)!
+    }
+    
+    @objc public class var rm_white: UIColor {
+        return UIColor(named: #function, in: .main, compatibleWith: nil)!
+    }
+}
